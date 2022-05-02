366ae712411e76c65d8c4d094048cf004bc5e836
jpmuerte <jpnicolet@gmail.com>
2022-05-02 22:12:41 +0200

    Modification des fichiers: importation_dump env
    test_connection: requête remplacée
    
    Modif. de la requête "SELECT" dans les fichiers gestion_genres_crud.pygenres

--- APP_FILMS_164/genres/gestion_genres_crud.py
+++ APP_FILMS_164/genres/gestion_genres_crud.py
@@ -174,7 +174,7 @@ def genre_update_wtf():
             return redirect(url_for('genres_afficher', order_by="ASC", id_genre_sel=id_genre_update))
         elif request.method == "GET":
             # Opération sur la BD pour récupérer "id_genre" et "intitule_genre" de la "t_genre"
-            str_sql_id_genre = "SELECT id_genre, intitule_genre FROM t_genre WHERE id_genre = %(value_id_genre)s"
+            str_sql_id_genre = "SELECT id_adresse, nom_adresse FROM t_adresse WHERE id_adresse = %(value_id_adresse)s"
             valeur_select_dictionnaire = {"value_id_genre": id_genre_update}
             with DBconnection() as mybd_conn:
                 mybd_conn.execute(str_sql_id_genre, valeur_select_dictionnaire)
